efaea17926451f525adb513b10c2e2cf07971c03
lucas23213 <fl755315@gmail.com>
2024-06-22 10:18:40 +0900

    plugin js e css

--- www/lib/plugins.js
+++ www/lib/plugins.js
@@ -1,4 +1,22 @@
 // This is a JavaScript file
-$(document).on("click", "#camera",function(
+$(document).on("click", "#camera",function(){
+
+navigator.camera.getPicture(onSuccess, Onfail , { quality: 50,
+    destinationType: Camera.destinationType.FILE_URI
     
-){
+    correctOrientation: true,
+    saveToPhotoAlbum: true
+    
+    
+    });
+
+function onSuccess(imageURI) {
+    var image = document.getElementById('myImage');
+    image.src = imageURI;
+}
+
+function onFail(message) {
+    alert('Failed because:' + message);
+}
+
+});
